8e35680af0ba5e4d404d0d94de951da796776a45
krados01 <krados.chaos@gmail.com>
2016-02-04 13:11:53 +0100

    Create character.cs

---
+++ character.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class character : MonoBehaviour{
+    void Start(){
+    	//general functions.
+        //model 3D
+        //sprites
+
+    }
+
+}
